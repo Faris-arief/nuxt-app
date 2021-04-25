393b22ad325511399babb6384eb0f6fe85409c98
Faris-arief <faris.arief98@gmail.com>
2021-04-26 01:17:17 +1000

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  loading: {color: '#39b982'},
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -32,6 +33,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create Event</h1>
+  </div>
+</template>
+<script>
+export default{
+  head(){
+    return{
+      title: 'Create Event',
+      meta: [
+        {
+          hid:'description',
+          name:'create events',
+          content: 'create events',
+        }
+      ]
+    }
+  }
+}
+</script>
